25a52193f68f22eda9d0afd9b89bb08e6588ee16
Axel Gohier <axel.gohier@etudiant.univ-lr.fr>
2023-05-23 13:35:13 +0200

    Fix: models/client.py missing Models

--- src/parser/models/client.py
+++ src/parser/models/client.py
@@ -2,9 +2,36 @@ from .base import Base_model
 from .session import Session_Model
 
 
-class Client_Model(Base_model):
+class Client_Base_Model(Base_model):
     client_id: str
     country: str = None
     city: str = None
     user_agent: str = None
+
+
+class Client_Model(Client_Base_Model):
     sessions: list[Session_Model] = []
+
+    def to_json(self):
+        sessions = []
+        for session in self.sessions:
+            sessions.append(session.to_json())
+        return {
+            "client_id": self.client_id,
+            "country": self.country,
+            "city": self.city,
+            "user_agent": self.user_agent,
+            "sessions": sessions
+        }
+
+
+class Client_Get_Model(Client_Base_Model):
+    class Config:
+        schema_extra = {
+            "example": {
+                "client_id": "self.client_id",
+                "country": "self.country",
+                "city": "self.city",
+                "user_agent": "self.user_agent",
+            }
+        }
