be5622dcef1a7c92ef73feb01b70655afe4ac757
Axel Gohier <axel.gohier@etudiant.univ-lr.fr>
2023-05-23 16:50:18 +0200

    Fix: tests utilisation utf-8

--- src/parser/main.py
+++ src/parser/main.py
@@ -1,5 +1,6 @@
 # Import necessary modules
 import re
+import codecs
 from decimal import Decimal
 
 from apachelogs import LogParser
@@ -163,15 +164,17 @@ async def compute(file, collection: list, parameters: Parameters):
     exclude_keywords = parameters.exclude_keywords
 
     # Create a parser object
-    parser = LogParser(parser_format, encoding="utf-8")
+    parser = LogParser(parser_format)
 
     # get the clients from the collection (if any)
     dict_client = {}
     for client in collection:
         dict_client[client.client_id] = client
 
-    with open(file, "r", encoding='utf-8') as f:
+    with open(file, "r", encoding='utf-8', errors='replace') as f:
         for entry in f:
+            # Encode the line in latin1 and decode it in utf-8
+            entry = entry.encode('latin1').decode('utf-8', errors='replace')
             # Replace spaces with underscores in the relevant portion of the line
             entry = replace_space_with_underscore(entry)
             # Check if the line is valuable
