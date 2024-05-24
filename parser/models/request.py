2af636e4e3549385891eefc057f2ac3c7e50f954
Ania DAHACHE <ania.dahache@univ-lr.fr>
2024-05-24 16:43:56 +0200

    Update: add cluster_id in parser

--- parser/models/request.py
+++ parser/models/request.py
@@ -1,5 +1,6 @@
 from .base import Base_model
 from datetime import datetime
+from typing import Optional
 
 
 class Request_Model(Base_model):
@@ -8,3 +9,4 @@ class Request_Model(Base_model):
     request_url: str | None = None
     response_code: str | None = None
     referer: str | None = None
+    cluster_id:  Optional[int] = None
